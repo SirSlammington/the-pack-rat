be8243081834d4be2dd3051a108295ea23325858
SirSlammington <44537892+SirSlammington@users.noreply.github.com>
2021-05-23 06:34:28 -0700

    Initial commit.
    Contains only the basic skeleton: POC, unfinished README, etc.

---
+++ Archive/thepackratPOC.py
@@ -0,0 +1,58 @@
+print(
+'''
+ _____ _           ______          _     ______      _   
+|_   _| |          | ___ \        | |    | ___ \    | |  
+  | | | |__   ___  | |_/ /_ _  ___| | __ | |_/ /__ _| |_ 
+  | | | '_ \ / _ \ |  __/ _` |/ __| |/ / |    // _` | __|
+  | | | | | |  __/ | | | (_| | (__|   <  | |\ \ (_| | |_ 
+  \_/ |_| |_|\___| \_|  \__,_|\___|_|\_\ \_| \_\__,_|\__|
+                                                         
+''')
+
+# Necessary modules
+import requests, re, os
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+# URL (I used 'https://greshamoregon.gov/City-Directory/')
+url = input('URL to scrape emails from: ')
+
+# Checks to see if the input was correct
+# Pretty awful input validation, but it works
+if 'http://' in url or 'https://' in url:
+  pass
+else:
+  url = 'http://' + url
+
+# Emails
+emails = []
+
+# Email regex
+'''
+This took a long time | Raw strings would've been a very useful tool to know a long time ago
+Got help from http://www.regexone.com/ and https://emailregex.com/
+'''
+pattern = r'(^[\w.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.])'
+regex = re.compile(pattern)
+
+# Sends an HTTP GET request, stores the HTTP response and soupifies the content of the response
+res = requests.get(url)
+soup = BeautifulSoup(res.content, 'html.parser')
+
+''' Finds every email addr on the page '''
+for email in soup.find_all(string=regex):
+  emails.append(email)
+
+# Print List to the console & write each email to a file
+resultFile = open(f'PackRat_{datetime.now().strftime("%H-%M-%S")}_EmailList.txt', 'w')
+
+# Loop to print and write
+for email in emails:
+  print(email)
+  resultFile.write(email + '\n')
+
+# Print location of the file
+print(os.get)
+
+# Close the file
+resultFile.close()
---
+++ README.md
@@ -0,0 +1,4 @@
+# The Pack Rat v1.0
+> The Pack Rat is a basic web scraping utility that can be used to fetch and compile information like emails, phone numbers, images, and various other media that renders on a targetted URL page. Note that this will *only* fetch targets that render on a web page—if you require a tool that can enumerate through an entire domain, you would do well to look for a spider program.  
+
+## Usage
